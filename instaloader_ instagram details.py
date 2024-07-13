7953bef82027cfd7dcd4ddb867af6581c11dc9c6
Niraj Kumar <152837781+niraj987@users.noreply.github.com>
2024-07-14 01:38:01 +0530

    Add files via upload
    Firstly 
    1. We Install Instaloader By Running    pip install instaloader  
    2. Check Version of instaloader
    3. Run Your Program

---
+++ instaloader_ instagram details.py
@@ -0,0 +1,19 @@
+# instagram details 
+import instaloader
+
+# creating an Instaloader() object
+ig=instaloader.Instaloader()
+
+# Taking the instagram username as input from user
+usrname=input("Enter username:")
+
+#Fetching the details of provided useraname using instaloder object
+profile=instaloader.Profile.from_username(ig.context, usrname)
+
+# Printing the fetched details and storing the profile pic of that account
+print("Username: ", profile.username)
+print("Number of Posts Uploaded: ", profile.mediacount)
+print(profile.username+" is having " + str(profile.followers)+' followers.')
+print(profile.username+" is following " + str(profile.followees)+' people')
+print("Bio: ", profile.biography)
+instaloader.Instaloader().download_profile(usrname,profile_pic_only=True)
